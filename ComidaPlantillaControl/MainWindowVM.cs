37c242e6d2c5f581c23887ed0a95be12d6ac0e99
MANUEL MADRIGAL LOSA <m.madrigallosa@edu.gva.es>
2023-12-03 22:22:32 +0100

    Agregar archivos de proyecto.

---
+++ ComidaPlantillaControl/MainWindowVM.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComidaPlantillaControl
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<string> tiposComida;
+
+        public ObservableCollection<string> TiposComida
+        {
+            get { return tiposComida; }
+            set
+            {
+                tiposComida = value;
+                NotifyPropertyChanged("TiposComida");
+            }
+        }
+
+        private ObservableCollection<Plato> listaPlatos;
+
+        public ObservableCollection<Plato> ListaPlatos
+        {
+            get { return listaPlatos; }
+            set
+            {
+                listaPlatos = value;
+                NotifyPropertyChanged("ListaPlatos");
+            }
+        }
+
+        private Plato platoSeleccionado;
+
+        public Plato PlatoSeleccionado
+        {
+            get { return platoSeleccionado; }
+            set
+            {
+                platoSeleccionado = value;
+                NotifyPropertyChanged("PlatoSeleccionado");
+            }
+        }
+
+        public MainWindowVM()
+        {
+            // ListaPlatos = Plato.GetSamples(System.IO.Path.Combine(Environment.CurrentDirectory,"Imagenes"));
+            ListaPlatos = Plato.GetSamples("FotosPlatos");
+            TiposComida = new ObservableCollection<string> { "China", "Americana", "Mexicana" };
+        }
+
+        public void QuitarSeleccionado()
+        {
+            PlatoSeleccionado = null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
